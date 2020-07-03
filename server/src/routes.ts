1b4570b1741762f16ca726a426cf2350aba0d632
RicardoMGM <ricardo.manel@gmail.com>
2020-07-03 17:41:02 -0300

    Adição de validação do service

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import PointsController from './controllers/PointsController';
 import ItensController from './controllers/ItensController';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 
@@ -15,7 +16,26 @@ routes.get('/itens', itensController.index);
 
 //No insomnia para enviar arquivo binários como uma imagem tem que mudar de
 //Json para multpart form
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            itens: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
+    
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
